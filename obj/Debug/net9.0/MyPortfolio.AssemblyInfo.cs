3dd7c1b75c165dee17a4bb572e85066de51d0a19
idir <idir.bylka@yahoo.co.uk>
2025-08-22 18:57:28 +0200

    created Imprint and Privacy Policy

--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029496713a2a91496cbfc39cd72f76a26eed7df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1548a227c78ff156557704121ce09eef9fa8e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
